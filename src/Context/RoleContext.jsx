8bfbae09da55ebb3724d3dfd826d1b349c9315a1
BaraaEBrahiem <ibrahiembaraa@gmail.com>
2026-08-22 11:03:28 +0300

    remove polling

--- src/Context/RoleContext.jsx
+++ src/Context/RoleContext.jsx
@@ -1,21 +1,30 @@
-
 import { createContext, useState } from "react";
-//eslint-disable-next-line
+
 export const RoleContext = createContext();
 
 export const RoleProvider = ({ children }) => {
   const [roles, setRoles] = useState(() => {
     const stored = localStorage.getItem("roles");
-    return stored ? JSON.parse(stored) : [""]; 
+
+    return stored ? JSON.parse(stored) : [""];
   });
 
   const updateRoles = (newRoles) => {
     setRoles(newRoles);
-    localStorage.setItem("roles", JSON.stringify(newRoles));
+
+    localStorage.setItem(
+      "roles",
+      JSON.stringify(newRoles)
+    );
   };
 
   return (
-    <RoleContext.Provider value={{ roles, updateRoles }}>
+    <RoleContext.Provider
+      value={{
+        roles,
+        updateRoles,
+      }}
+    >
       {children}
     </RoleContext.Provider>
   );
